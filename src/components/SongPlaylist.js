17d971ae8bd1874c3a903f8237cabeabf6d15192
Bozhidar Gorchev <bgorchev@aiopsgroup.com>
2023-01-31 10:32:49 +0200

    add trining redux toolkit project

---
+++ src/components/SongPlaylist.js
@@ -0,0 +1,48 @@
+import { useDispatch, useSelector } from "react-redux";
+import { addSong, removeSong } from "../store";
+
+function SongPlaylist() {
+  const dispatch = useDispatch();
+
+  const songPlaylist = useSelector((state) => {
+    return state.songs;
+  });
+
+  const handleSongAdd = () => {
+    dispatch(addSong("doom"));
+  };
+
+  const handleSongRemove = () => {
+    dispatch(removeSong());
+  };
+
+  const renderedSongs = songPlaylist.map((song) => {
+    return (
+      <li key={song}>
+        {song}
+        <button
+          onClick={() => handleSongRemove(song)}
+          className="button is-danger"
+        >
+          X
+        </button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="content">
+      <div className="table-header">
+        <h3 className="subtitle is-3">Song Playlist</h3>
+        <div className="buttons">
+          <button onClick={handleSongAdd} className="button is-link">
+            + Add Song to Playlist
+          </button>
+        </div>
+      </div>
+      <ul>{renderedSongs}</ul>
+    </div>
+  );
+}
+
+export default SongPlaylist;
